f4e5dddd56e8d224de71fdc37bf7f97d68068142
NSri4112005 <nithyasriparivallal@gmail.com>
2026-09-13 10:55:10 +0530

    fix: register product routes in main router

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,33 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { authRoutes } from "./routes/auth.routes";
+import { productRoutes } from "./routes/product.routes";
 
 export const router = async (
   req: IncomingMessage,
   res: ServerResponse
 ): Promise<boolean> => {
-  const pathname = new URL(
+  const url = new URL(
     req.url || "/",
     `http://${req.headers.host || "localhost"}`
-  ).pathname;
+  );
+
+  const pathname = url.pathname;
+  const queryParams = url.searchParams;
 
   // Authentication routes
   if (await authRoutes(req, res, pathname)) {
+    return true;
+  }
+
+  // Product routes
+  if (
+    await productRoutes(
+      req,
+      res,
+      pathname,
+      queryParams
+    )
+  ) {
     return true;
   }
 
